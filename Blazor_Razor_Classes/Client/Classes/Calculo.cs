fecdf27b8fda1d06b8e45e42022d5ebbcf307a7d
João Paulo Medeiros Rocha <joaopaulo.rocha@aridesa.com.br>
2023-05-29 18:03:06 -0300

    Vendo funcionamenteo de classes no Razor

---
+++ Blazor_Razor_Classes/Client/Classes/Calculo.cs
@@ -0,0 +1,7 @@
+namespace Blazor_Razor_Classes.Client.Classes{
+    public class Calculo{
+        public static int Somar(int n1, int n2) => n1 + n2;
+        public static int Subtrair(int n1, int n2) => n1 - n2;
+        public static int Multiplicar(int n1, int n2) => n1 * n2;
+    }
+}
---
+++ Blazor_Razor_Classes/Shared/Entidades/Produto.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Blazor_Razor_Classes.Shared.Entidades{
+
+    public class Produto{
+        public string Nome { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+
+}
